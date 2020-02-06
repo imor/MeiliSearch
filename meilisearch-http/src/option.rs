244f4011d150128c14a54ce937b758ab70732523
qdequele <quentin@dequelen.me>
2020-02-06 15:42:35 +0100

    add launch resume & environment

--- meilisearch-http/src/option.rs
+++ meilisearch-http/src/option.rs
@@ -14,7 +14,10 @@ pub struct Opt {
     #[structopt(long, env = "MEILI_MASTER_KEY")]
     pub master_key: Option<String>,
 
-    /// Do not send analytics to Meili.
-    #[structopt(long, env = "MEILI_NO_ANALYTICS")]
-    pub no_analytics: bool,
+    /// This environment variable must be set to `production` if your are running in production.
+    /// Could be `production` or `development`
+    /// - `production`: Force api keys
+    /// - `development`: Show logs in "info" mode + not mendatory to specify the api keys
+    #[structopt(long, env = "MEILI_ENV", default_value = "development")]
+    pub env: String,
 }
